3a070f16867c1c5499fdd3b444c7e32aa1a47263
root <root@paladin.myxingfu.net>
2017-02-26 13:50:25 -0500

    Added bootstrap cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,8 @@
 # Copyright (c) 2016 ESN Xingfu, All Rights Reserved.
 # Logic comes from sensu/sensu-chef
 
+include_recipe 'esnbootstrap::default'
+
 include_recipe 'sensu::redis'
 
 include_recipe 'sensu::rabbit'
